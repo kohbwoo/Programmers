b3cd4c0999546cc955450a5b666a5ae8d1a4500b
kohbwoo <kohbwoo@gmail.com>
2021-12-20 02:41:50 +0900

    commit

---
+++ Level 2/60057.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = list(s)
+    tmp = []
+    for i in range(1, len(s)):
+        count = 0
+        while(s[i] == s[i-1]):
+            if s[i] == s[i-1]:
+                count += 1
+            i += 1
+        tmp.append(count)
+
+    return answer
+
+
+print(solution("aabbaccc"))
+#print(solution("ababcdcdababcdcd"))
+# print(solution("abcabcdede"))
+# print(solution("abcabcabcabcdededededede"))
+# print(solution("xababcdcdababcdcd"))

981da95e523eec31ad788c7e95126e1193cad429
kohbwoo <kohbwoo@gmail.com>
2021-12-19 16:52:32 +0900

    commit

--- Level 1/77884.py
+++ Level 1/77884.py
@@ -2,18 +2,18 @@ def solution(left, right):
     tmp = []
     count = []
     answer = 0
-    for i in range(left,right+1):
+    for i in range(left, right + 1):
         tmp.append(i)
     for j in range(0, len(tmp)):
         for a in range(1, tmp[j] + 1):
-            if tmp[j] % a == 0:
+            if not tmp[j] % a:
                 count.append(a)
-        if len(count) % 2 == 0:
-            answer = answer + a
+        if not len(count) % 2:
+            answer += a
         else:
-            answer = answer - a
+            answer -= a
         count = []
     return answer
 
 
-print(solution(13,17))
+print(solution(13, 17))

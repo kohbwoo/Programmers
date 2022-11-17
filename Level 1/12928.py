2253168fcd0b593f246ea89469cbdedac2353b92
ByungWoo Koh <kohbwoo@gmail.com>
2022-11-17 12:24:33 +0900

    .

---
+++ Level 1/12928.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n):
+        if n%i == 0:
+            answer += i
+    answer += n
+    return answer
+
+
+print(solution(12))
+print(solution(5))
